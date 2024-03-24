8f221ab455c846ae691b136598a35e4a34620e9c
Kshitij Rijal <kshitijrijal@Kshitijs-MacBook-Air.local>
2024-03-23 19:23:14 -0600

    Final Submission

---
+++ src/javascripts/images.js
@@ -0,0 +1,77 @@
+export const animalImages = [
+    {
+      "id": 1,
+      "title": "Polar Bear",
+      "URL": "https://images.unsplash.com/photo-1589656966895-2f33e7653819?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Polar bears are stocky, with a long neck, relatively small head, short, rounded ears, and a short tail. The male, which is much larger than the female, weighs 410 to 720 kg (900 to 1,600 pounds). It grows to about 1.6 metres (5.3 feet) tall at the shoulder and 2.2–2.5 metres in length."
+    },
+    {
+      "id": 2,
+      "title": "Ring Tailed Lemur",
+      "URL": "https://images.unsplash.com/photo-1574949717565-ed7a7e95e32d?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Ring-tailed lemur backs are gray to rosy brown with gray limbs and dark gray heads and necks. They have white bellies. Their faces are white with dark triangular eye patches and a black nose. True to their name, ring-tailed lemurs' tails are ringed with 13 alternating black and white bands."
+    },
+    {
+      "id": 3,
+      "title": "Penguin",
+      "URL": "https://images.unsplash.com/photo-1574950333594-f3e9a9446d0f?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "A group of Emporer Penguins at Calgary Zoo, Alberta. They have a large head, short neck, and elongated body. Their tails are short, stiff, and wedge-shaped. Their legs and webbed feet are set far back on the body, which gives penguins their upright posture on land. When snow conditions are right, they will slide on their bellies."
+    },
+    {
+      "id": 4,
+      "title": "Squirrels",
+      "URL": "https://images.unsplash.com/photo-1571313718941-dbec371e5de1?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Squirrels are generally small rodents with slender bodies, bushy tails and large eyes. In general, their fur is short, soft and silky, and ranges in thickness from species to species. The color of their fur is also highly variable and can be whitish, gray, yellow, red, brown, or even black."
+    },
+    {
+      "id": 5,
+      "title": "Wolf",
+      "URL": "https://images.unsplash.com/photo-1511530155862-18a92c0691d9?q=80&w=1748&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Like most canids, wolves have a high body, long legs, broad skull tapering to a narrow muzzle. The tail is bushy and coat has a thick, dense underfur. Colors vary from light to dark gray with black and white interspersed, to some individuals being solid black and solid white."
+    },
+    {
+      "id": 6,
+      "title": "Tiger",
+      "URL": "https://images.unsplash.com/photo-1549480017-d76466a4b7e8?q=80&w=1712&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Tigers are powerful hunters with sharp teeth, strong jaws and agile bodies. They are the largest terrestrial mammal whose diet consists entirely of meat; the largest tiger ever recorded was an Amur tiger. The tiger's closest relative is the lion. In fact, without fur, it is difficult to distinguish a tiger from a lion."
+    },
+    {
+      "id": 7,
+      "title": "Komodo Dragon",
+      "URL": "https://images.unsplash.com/photo-1590691563977-97e10916647b?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Komodo dragons are large lizards with long tails, strong and agile necks, and sturdy limbs. Their tongues are yellow and forked. Adults are an almost-uniform stone color with distinct, large scales, while juveniles may display a more vibrant color and pattern."
+    },
+    
+    
+    {
+      "id": 8,
+      "title": "Corcodile",
+      "URL": "https://images.unsplash.com/photo-1600333489678-6c6a1f0269a7?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Crocodiles have powerful jaws with many conical teeth and short legs with clawed webbed toes. They share a unique body form that allows the eyes, ears, and nostrils to be above the water surface while most of the animal is hidden below. The tail is long and massive, and the skin is thick and plated."
+    },
+    {
+      "id": 9,
+      "title": "Green Snake",
+      "URL": "https://images.unsplash.com/photo-1600333433600-8206247d6c99?q=80&w=1724&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Macro photo of a Green Tree Snake at Hartleys Crocodile Adventures. Dendrelaphis punctulatus. green snake, any of several species belonging to the family Colubridae, named for their colour. The North American green snakes are the two species of the genus Opheodrys. These docile, slender, harmless snakes often live in gardens. They lay eggs, and they subsist on insects and spiders."
+    },
+    {
+      "id": 10,
+      "title": "Turtle",
+      "URL": "https://images.unsplash.com/photo-1597162216923-ba6d99390c10?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "A turtle is a reptile that has a shell covering its body. Turtles are known for moving very slowly. There are more than 350 species, or types, of turtle. Turtles are found in most parts of the world."
+    },
+    {
+      "id": 11,
+      "title": "Lizard",
+      "URL": "https://images.unsplash.com/photo-1551946581-f7a62cd2f00b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "Green crested lizard in Singapore, In general, lizards have a small head, short neck, and long body and tail. Unlike snakes, most lizards have moveable eyelids. There are currently over 4,675 lizard species, including iguanas, chameleons, geckos, Gila monsters, monitors, and skinks."
+    },
+    {
+      "id": 12,
+      "title": "Chaffinch",
+      "URL": "https://images.unsplash.com/photo-1493236296276-d17357e28888?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "description": "The male chaffinch is one of the most colourful garden birds with a blue-grey crown, brown back and pink breast. Females are brown, but are less streaky than female house sparrows, and have white shoulder patches and wingbars."
+    }
+  ];
+  
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,36 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+import { animalImages } from "./images"
+
+for(let g of animalImages){
+    let g_thumb = document.getElementById('g'+ g.id)
+    g_thumb.innerHTML = `
+    <img src="${g.URL}" alt="${g.title}" class="img-thumbnail"/>
+    `
+    g_thumb.onclick = function() {
+        displayImage(g);
+    }
+    
+}
+
+
+
+let featured_image = document.querySelector(".featured");
+// default image
+displayImage(animalImages[0]);
+
+function displayImage(image) {
+    featured_image.innerHTML = `
+        <div class="card">
+            <div class="card-header">${image.title}</div>
+            <img src="${image.URL}" class="card-img-top" alt="${image.title}">
+            <div class="card-body">
+            ${image.description}
+            </div>
+            
+        </div>
+    `;
+}
+
+
